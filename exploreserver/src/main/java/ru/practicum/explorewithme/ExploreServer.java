17967e5b21a849ce3d29171bcaed51ac029b7412
Ilia28593 <111430909+Ilia28593@users.noreply.github.com>
2023-11-01 12:41:15 +0300

    add stat service

--- exploreserver/src/main/java/ru/practicum/explorewithme/ExploreServer.java
+++ exploreserver/src/main/java/ru/practicum/explorewithme/ExploreServer.java
@@ -1,11 +1,12 @@
-package ru.practicum;
+package ru.practicum.explorewithme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExploreWithMeEwm {
+public class ExploreServer {
     public static void main(String[] args) {
-        SpringApplication.run(ExploreWithMeEwm.class, args);
+        SpringApplication.run(ExploreServer.class, args);
     }
+
 }
---
+++ statserver/src/main/java/ru/practicum/explorewithme/constants/Constant.java
@@ -0,0 +1,5 @@
+package ru.practicum.explorewithme.constants;
+
+public class Constant {
+    public static final String DATA_FORMAT_PATTERN = "yyyy-MM-dd HH:mm:ss";
+}
