fd3c3e28ed617010810a1a817e21089229c80c45
Ilia28593 <111430909+Ilia28593@users.noreply.github.com>
2023-11-02 18:34:23 +0300

    add stat service

--- stats/stats-client/src/main/java/ru/practicum/client/StatClientImpl.java
+++ stats/stats-client/src/main/java/ru/practicum/client/StatClientImpl.java
@@ -22,41 +22,49 @@ public class StatClientImpl implements StatClient {
     @Value("${stats-server.url}")
     private final String serverURL;
     private final RestTemplate restTemplate;
+    private final HttpHeaders headers;
 
-    private HttpHeaders getHeader() {
-        HttpHeaders headers = new HttpHeaders();
+    private void prepareHeader() {
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        return headers;
     }
 
     @Override
     public ResponseEntity<String> saveHit(String app, String uri, String ip, LocalDateTime timestamp) {
+        prepareHeader();
+
+        EndpointDto endpointDto = EndpointDto.builder()
+                .app(app)
+                .uri(uri)
+                .ip(ip)
+                .timestamp(timestamp)
+                .build();
+
+        HttpEntity<EndpointDto> entity = new HttpEntity<>(endpointDto, headers);
+
         return this.restTemplate.postForEntity(
                 serverURL + "/hit",
-                new HttpEntity<>(
-                        new EndpointDto()
-                                .setUri(uri)
-                                .setIp(ip)
-                                .setApp(app)
-                                .setTimestamp(timestamp),
-                        getHeader()),
+                entity,
                 String.class);
     }
 
     @SneakyThrows
     @Override
     public ResponseEntity<List<StatsDto>> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
+        prepareHeader();
+
         Map<String, Object> params = new HashMap<>();
         params.put("start", start);
         params.put("end", end);
         params.put("uris", uris);
         params.put("unique", unique);
 
+        HttpEntity request = new HttpEntity(headers);
+
         return restTemplate.exchange(
                 serverURL + "/stats?start={start}&end={end}&uris={uris}&unique={unique}",
                 HttpMethod.GET,
-                new HttpEntity<>(getHeader()),
+                request,
                 new ParameterizedTypeReference<>() {
                 },
                 params
