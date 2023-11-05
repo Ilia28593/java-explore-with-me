efc113708a325d00e0f72d334210ce7d1a1852c3
Ilia28593 <111430909+Ilia28593@users.noreply.github.com>
2023-11-05 15:30:37 +0300

    add stat service

---
+++ ewm-service/src/main/java/ru/practicum/ewm/request/ParticipationRequest.java
@@ -0,0 +1,50 @@
+package ru.practicum.ewm.request;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.*;
+import lombok.experimental.Accessors;
+import org.hibernate.annotations.CreationTimestamp;
+import ru.practicum.ewm.event.Event;
+import ru.practicum.ewm.request.enums.ParticipationRequestStatus;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "requests", schema = "public")
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class ParticipationRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    private LocalDateTime created;
+
+    @Column(nullable = false)
+    private Long requesterId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ParticipationRequestStatus status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id")
+    @JsonBackReference
+    private Event event;
+
+    public static ParticipationRequest of(Long userId, Long eventId) {
+       return new ParticipationRequest()
+                .setRequesterId(userId)
+                .setId(eventId)
+                .setEvent(new Event())
+                .setStatus(ParticipationRequestStatus.PENDING);
+    }
+}
---
+++ ewm-service/src/main/java/ru/practicum/ewm/user/User.java
@@ -0,0 +1,22 @@
+package ru.practicum.ewm.user;
+
+@Entity
+@Table(name = "users", schema = "public")
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+}
