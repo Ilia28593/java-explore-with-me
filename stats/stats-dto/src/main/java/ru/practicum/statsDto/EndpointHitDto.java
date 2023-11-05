3507ae7a2ed2a94acb3413eda383faa62c724aba
Ilia28593 <111430909+Ilia28593@users.noreply.github.com>
2023-11-05 19:05:15 +0300

    add stat service

--- stats/stats-dto/src/main/java/ru/practicum/statsDto/EndpointHitDto.java
+++ stats/stats-dto/src/main/java/ru/practicum/statsDto/EndpointHitDto.java
@@ -1,11 +1,6 @@
 package ru.practicum.statsDto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import lombok.experimental.Accessors;
 
 
