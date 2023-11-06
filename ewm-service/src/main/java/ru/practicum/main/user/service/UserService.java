f44823b9fdf6caa2c36d9b98a1481382df2ca17e
Ilia28593 <111430909+Ilia28593@users.noreply.github.com>
2023-11-06 15:21:44 +0300

    add stat service

--- ewm-service/src/main/java/ru/practicum/main/user/service/UserService.java
+++ ewm-service/src/main/java/ru/practicum/main/user/service/UserService.java
@@ -4,7 +4,6 @@ import ru.practicum.main.user.dto.NewUserRequest;
 import ru.practicum.main.user.dto.UserDto;
 
 import java.util.Collection;
-import java.util.List;
 
 public interface UserService {
     UserDto addUserAdmin(NewUserRequest newUserRequest);
