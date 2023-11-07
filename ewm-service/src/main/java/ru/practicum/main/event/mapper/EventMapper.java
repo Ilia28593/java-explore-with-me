d874e9f07b71cd02181e23813aa7cb82ecc7d477
Ilia28593 <111430909+Ilia28593@users.noreply.github.com>
2023-11-07 21:46:57 +0300

    add stat service

--- ewm-service/src/main/java/ru/practicum/main/event/mapper/EventMapper.java
+++ ewm-service/src/main/java/ru/practicum/main/event/mapper/EventMapper.java
@@ -3,11 +3,7 @@ package ru.practicum.main.event.mapper;
 
 import ru.practicum.main.category.mapper.CategoryMapper;
 import ru.practicum.main.category.model.Category;
-import ru.practicum.main.event.dto.EventFullDto;
-import ru.practicum.main.event.dto.EventShortDto;
-import ru.practicum.main.event.dto.NewEventDto;
-import ru.practicum.main.event.dto.UpdateEventAdminRequest;
-import ru.practicum.main.event.dto.UpdateEventUserRequest;
+import ru.practicum.main.event.dto.*;
 import ru.practicum.main.event.model.Event;
 import ru.practicum.main.event.model.State;
 import ru.practicum.main.location.mapper.LocationMapper;
@@ -21,96 +17,91 @@ import static ru.practicum.main.constant.Constants.DATE_FORMAT;
 
 public class EventMapper {
     public static EventFullDto toEventFullDto(Event event) {
-        return EventFullDto.builder()
-                .id(event.getId())
-                .annotation(event.getAnnotation())
-                .category(CategoryMapper.toCategoryDto(event.getCategory()))
-                .confirmedRequests(event.getConfirmedRequests())
-                .createdOn(event.getCreatedOn().format(DateTimeFormatter.ofPattern(DATE_FORMAT)))
-                .description(event.getDescription())
-                .eventDate(event.getEventDate().format(DateTimeFormatter.ofPattern(DATE_FORMAT)))
-                .initiator(UserMapper.toUserShortDto(event.getInitiator()))
-                .location(LocationMapper.toLocationDto(event.getLocation()))
-                .paid(event.getPaid())
-                .participantLimit(event.getParticipantLimit())
-                .publishedOn(event.getPublishedOn() == null ? null : event.getPublishedOn().format(DateTimeFormatter.ofPattern(DATE_FORMAT)))
-                .requestModeration(event.getRequestModeration())
-                .state(event.getState() == null ? null : event.getState().toString())
-                .title(event.getTitle())
-                .views(event.getViews())
-                .build();
+        return new EventFullDto()
+                .setId(event.getId())
+                .setAnnotation(event.getAnnotation())
+                .setCategory(CategoryMapper.toCategoryDto(event.getCategory()))
+                .setConfirmedRequests(event.getConfirmedRequests())
+                .setCreatedOn(event.getCreatedOn().format(DateTimeFormatter.ofPattern(DATE_FORMAT)))
+                .setDescription(event.getDescription())
+                .setEventDate(event.getEventDate().format(DateTimeFormatter.ofPattern(DATE_FORMAT)))
+                .setInitiator(UserMapper.toUserShortDto(event.getInitiator()))
+                .setLocation(LocationMapper.toLocationDto(event.getLocation()))
+                .setPaid(event.getPaid())
+                .setParticipantLimit(event.getParticipantLimit())
+                .setPublishedOn(event.getPublishedOn() == null ? null : event.getPublishedOn().format(DateTimeFormatter.ofPattern(DATE_FORMAT)))
+                .setRequestModeration(event.getRequestModeration())
+                .setState(event.getState() == null ? null : event.getState().toString())
+                .setTitle(event.getTitle())
+                .setViews(event.getViews());
     }
 
     public static EventShortDto toEventShortDto(Event event) {
-        return EventShortDto.builder()
-                .id(event.getId())
-                .annotation(event.getAnnotation())
-                .category(CategoryMapper.toCategoryDto(event.getCategory()))
-                .confirmedRequests(event.getConfirmedRequests())
-                .eventDate(event.getEventDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
-                .initiator(UserMapper.toUserShortDto(event.getInitiator()))
-                .paid(event.getPaid())
-                .title(event.getTitle())
-                .views(event.getViews())
-                .build();
+        return new EventShortDto()
+                .setId(event.getId())
+                .setAnnotation(event.getAnnotation())
+                .setCategory(CategoryMapper.toCategoryDto(event.getCategory()))
+                .setConfirmedRequests(event.getConfirmedRequests())
+                .setEventDate(event.getEventDate().format(DateTimeFormatter.ofPattern(DATE_FORMAT)))
+                .setInitiator(UserMapper.toUserShortDto(event.getInitiator()))
+                .setPaid(event.getPaid())
+                .setTitle(event.getTitle())
+                .setViews(event.getViews());
     }
 
     public static Event toEvent(NewEventDto newEventDto, User initiator, Category category) {
-        return Event.builder()
-                .annotation(newEventDto.getAnnotation())
-                .category(category)
-                .createdOn(LocalDateTime.now())
-                .description(newEventDto.getDescription())
-                .eventDate(LocalDateTime.parse(newEventDto.getEventDate(), DateTimeFormatter.ofPattern(DATE_FORMAT)))
-                .initiator(initiator)
-                .location(newEventDto.getLocation())
-                .confirmedRequests(0L)
-                .paid(newEventDto.getPaid() == null ? false : newEventDto.getPaid())
-                .participantLimit(newEventDto.getParticipantLimit() == null ? 0 : newEventDto.getParticipantLimit())
-                .requestModeration(newEventDto.getRequestModeration() == null ? true : newEventDto.getRequestModeration())
-                .state(State.PENDING)
-                .title(newEventDto.getTitle())
-                .views(0L)
-                .build();
+        return new Event()
+                .setAnnotation(newEventDto.getAnnotation())
+                .setCategory(category)
+                .setCreatedOn(LocalDateTime.now())
+                .setDescription(newEventDto.getDescription())
+                .setEventDate(LocalDateTime.parse(newEventDto.getEventDate(), DateTimeFormatter.ofPattern(DATE_FORMAT)))
+                .setInitiator(initiator)
+                .setLocation(newEventDto.getLocation())
+                .setConfirmedRequests(0L)
+                .setPaid(newEventDto.getPaid() == null ? false : newEventDto.getPaid())
+                .setParticipantLimit(newEventDto.getParticipantLimit() == null ? 0 : newEventDto.getParticipantLimit())
+                .setRequestModeration(newEventDto.getRequestModeration() == null ? true : newEventDto.getRequestModeration())
+                .setState(State.PENDING)
+                .setTitle(newEventDto.getTitle())
+                .setViews(0L);
     }
 
     public static Event toEvent(UpdateEventAdminRequest updateEventAdminRequest, Event oldEvent, Category category) {
-        return Event.builder()
-                .id(oldEvent.getId())
-                .annotation(updateEventAdminRequest.getAnnotation() == null ? oldEvent.getAnnotation() : updateEventAdminRequest.getAnnotation())
-                .category(updateEventAdminRequest.getCategory() == null ? oldEvent.getCategory() : category)
-                .confirmedRequests(oldEvent.getConfirmedRequests())
-                .createdOn(oldEvent.getCreatedOn())
-                .description(updateEventAdminRequest.getDescription() == null ? oldEvent.getDescription() : updateEventAdminRequest.getDescription())
-                .eventDate(updateEventAdminRequest.getEventDate() == null ? oldEvent.getEventDate() : LocalDateTime.parse(updateEventAdminRequest.getEventDate(), DateTimeFormatter.ofPattern(DATE_FORMAT)))
-                .initiator(oldEvent.getInitiator())
-                .location(updateEventAdminRequest.getLocation() == null ? oldEvent.getLocation() : updateEventAdminRequest.getLocation())
-                .paid(updateEventAdminRequest.getPaid() == null ? oldEvent.getPaid() : updateEventAdminRequest.getPaid())
-                .participantLimit(updateEventAdminRequest.getParticipantLimit() == null ? oldEvent.getParticipantLimit() : updateEventAdminRequest.getParticipantLimit())
-                .requestModeration(updateEventAdminRequest.getRequestModeration() == null ? oldEvent.getRequestModeration() : updateEventAdminRequest.getRequestModeration())
-                .state(oldEvent.getState())
-                .title(updateEventAdminRequest.getTitle() == null ? oldEvent.getTitle() : updateEventAdminRequest.getTitle())
-                .views(oldEvent.getViews())
-                .build();
+        return new Event()
+                .setId(oldEvent.getId())
+                .setAnnotation(updateEventAdminRequest.getAnnotation() == null ? oldEvent.getAnnotation() : updateEventAdminRequest.getAnnotation())
+                .setCategory(updateEventAdminRequest.getCategory() == null ? oldEvent.getCategory() : category)
+                .setConfirmedRequests(oldEvent.getConfirmedRequests())
+                .setCreatedOn(oldEvent.getCreatedOn())
+                .setDescription(updateEventAdminRequest.getDescription() == null ? oldEvent.getDescription() : updateEventAdminRequest.getDescription())
+                .setEventDate(updateEventAdminRequest.getEventDate() == null ? oldEvent.getEventDate() : LocalDateTime.parse(updateEventAdminRequest.getEventDate(), DateTimeFormatter.ofPattern(DATE_FORMAT)))
+                .setInitiator(oldEvent.getInitiator())
+                .setLocation(updateEventAdminRequest.getLocation() == null ? oldEvent.getLocation() : updateEventAdminRequest.getLocation())
+                .setPaid(updateEventAdminRequest.getPaid() == null ? oldEvent.getPaid() : updateEventAdminRequest.getPaid())
+                .setParticipantLimit(updateEventAdminRequest.getParticipantLimit() == null ? oldEvent.getParticipantLimit() : updateEventAdminRequest.getParticipantLimit())
+                .setRequestModeration(updateEventAdminRequest.getRequestModeration() == null ? oldEvent.getRequestModeration() : updateEventAdminRequest.getRequestModeration())
+                .setState(oldEvent.getState())
+                .setTitle(updateEventAdminRequest.getTitle() == null ? oldEvent.getTitle() : updateEventAdminRequest.getTitle())
+                .setViews(oldEvent.getViews());
     }
 
     public static Event toEvent(UpdateEventUserRequest updateEventUserRequest, Event oldEvent, Category category) {
-        return Event.builder()
-                .id(oldEvent.getId())
-                .annotation(updateEventUserRequest.getAnnotation() == null ? oldEvent.getAnnotation() : updateEventUserRequest.getAnnotation())
-                .category(updateEventUserRequest.getCategory() == null ? oldEvent.getCategory() : category)
-                .confirmedRequests(oldEvent.getConfirmedRequests())
-                .createdOn(oldEvent.getCreatedOn())
-                .description(updateEventUserRequest.getDescription() == null ? oldEvent.getDescription() : updateEventUserRequest.getDescription())
-                .eventDate(updateEventUserRequest.getEventDate() == null ? oldEvent.getEventDate() : LocalDateTime.parse(updateEventUserRequest.getEventDate(), DateTimeFormatter.ofPattern(DATE_FORMAT)))
-                .initiator(oldEvent.getInitiator())
-                .location(updateEventUserRequest.getLocation() == null ? oldEvent.getLocation() : updateEventUserRequest.getLocation())
-                .paid(updateEventUserRequest.getPaid() == null ? oldEvent.getPaid() : updateEventUserRequest.getPaid())
-                .participantLimit(updateEventUserRequest.getParticipantLimit() == null ? oldEvent.getParticipantLimit() : updateEventUserRequest.getParticipantLimit())
-                .requestModeration(updateEventUserRequest.getRequestModeration() == null ? oldEvent.getRequestModeration() : updateEventUserRequest.getRequestModeration())
-                .title(updateEventUserRequest.getTitle() == null ? oldEvent.getTitle() : updateEventUserRequest.getTitle())
-                .views(oldEvent.getViews())
-                .build();
+        return new Event()
+                .setId(oldEvent.getId())
+                .setAnnotation(updateEventUserRequest.getAnnotation() == null ? oldEvent.getAnnotation() : updateEventUserRequest.getAnnotation())
+                .setCategory(updateEventUserRequest.getCategory() == null ? oldEvent.getCategory() : category)
+                .setConfirmedRequests(oldEvent.getConfirmedRequests())
+                .setCreatedOn(oldEvent.getCreatedOn())
+                .setDescription(updateEventUserRequest.getDescription() == null ? oldEvent.getDescription() : updateEventUserRequest.getDescription())
+                .setEventDate(updateEventUserRequest.getEventDate() == null ? oldEvent.getEventDate() : LocalDateTime.parse(updateEventUserRequest.getEventDate(), DateTimeFormatter.ofPattern(DATE_FORMAT)))
+                .setInitiator(oldEvent.getInitiator())
+                .setLocation(updateEventUserRequest.getLocation() == null ? oldEvent.getLocation() : updateEventUserRequest.getLocation())
+                .setPaid(updateEventUserRequest.getPaid() == null ? oldEvent.getPaid() : updateEventUserRequest.getPaid())
+                .setParticipantLimit(updateEventUserRequest.getParticipantLimit() == null ? oldEvent.getParticipantLimit() : updateEventUserRequest.getParticipantLimit())
+                .setRequestModeration(updateEventUserRequest.getRequestModeration() == null ? oldEvent.getRequestModeration() : updateEventUserRequest.getRequestModeration())
+                .setTitle(updateEventUserRequest.getTitle() == null ? oldEvent.getTitle() : updateEventUserRequest.getTitle())
+                .setViews(oldEvent.getViews());
     }
 
 }
--- ewm-service/src/main/java/ru/practicum/main/event/model/Event.java
+++ ewm-service/src/main/java/ru/practicum/main/event/model/Event.java
@@ -1,25 +1,21 @@
 package ru.practicum.main.event.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
 import ru.practicum.main.category.model.Category;
 import ru.practicum.main.location.model.Location;
 import ru.practicum.main.user.model.User;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
 @Entity
-@Builder
+@Accessors(chain = true)
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "events")
