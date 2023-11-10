b24869c3e758192bf087233cc22a8860dcefe382
Ilia28593 <111430909+Ilia28593@users.noreply.github.com>
2023-11-10 18:07:24 +0300

    add main service

---
+++ stats/stats-server/src/main/java/ru/practicum/controller/StatController.java
@@ -0,0 +1,39 @@
+package ru.practicum.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.service.StatsService;
+import ru.practicum.statsDto.EndpointHitDto;
+import ru.practicum.statsDto.ViewStats;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+import static ru.practicum.constant.Constants.DATE_FORMAT;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class StatController {
+
+    private final StatsService statsService;
+
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/hit")
+    public EndpointHitDto saveHit(@RequestBody EndpointHitDto endpointHitDto) {
+        return statsService.createStatHit(endpointHitDto);
+    }
+
+    @GetMapping("/stats")
+    public List<ViewStats> getStats(@RequestParam @DateTimeFormat(pattern = DATE_FORMAT) LocalDateTime start,
+                                    @RequestParam @DateTimeFormat(pattern = DATE_FORMAT) LocalDateTime end,
+                                    @RequestParam(required = false) Collection<String> uris,
+                                    @RequestParam(defaultValue = "false") boolean unique) {
+        return statsService.getStatHit(start, end, uris, unique);
+    }
+}
