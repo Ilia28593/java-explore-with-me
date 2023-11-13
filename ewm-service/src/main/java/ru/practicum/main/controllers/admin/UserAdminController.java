a30693946678e149984573bef3d0bba7f5026e49
Ilia28593 <111430909+Ilia28593@users.noreply.github.com>
2023-11-13 10:53:53 +0300

    fix feature

--- ewm-service/src/main/java/ru/practicum/main/controllers/admin/UserAdminController.java
+++ ewm-service/src/main/java/ru/practicum/main/controllers/admin/UserAdminController.java
@@ -15,6 +15,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.PositiveOrZero;
 import java.util.Collection;
+
 @Slf4j
 @Validated
 @RestController
